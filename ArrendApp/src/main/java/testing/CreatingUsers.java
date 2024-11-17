8bc7ec1407c010482669f4aa92e8f9cf4bcd4181
Danieling <johandanielgs@ufps.edu.co>
2024-11-17 15:56:44 -0500

    changing  the type for the variable code from PhoneNumber class

--- ArrendApp/src/main/java/testing/CreatingUsers.java
+++ ArrendApp/src/main/java/testing/CreatingUsers.java
@@ -1,11 +1,20 @@
 package testing;
 
+import java.util.Scanner;
+
+import Model.User.PhoneNumber;
 import Model.User.User;
 import Model.User.User_Client;
 
 public class CreatingUsers {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
         User user1 = new User_Client(10101010, "Roberto", "Ambriosio", "+57 311 393 6341", "ambrosio@nodomain.com");
         System.out.println(user1);
+
+        PhoneNumber num = new PhoneNumber(scan.nextInt(), scan.nextInt());
+        System.out.println(num);
+
     }
 }
