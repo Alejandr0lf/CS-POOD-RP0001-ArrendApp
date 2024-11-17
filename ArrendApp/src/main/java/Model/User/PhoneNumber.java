94d3c79e1fe7acac3b612d153f33fc26a2957384
Danieling <johandanielgs@ufps.edu.co>
2024-11-17 16:01:29 -0500

    test

--- ArrendApp/src/main/java/Model/User/PhoneNumber.java
+++ ArrendApp/src/main/java/Model/User/PhoneNumber.java
@@ -2,22 +2,22 @@ package Model.User;
 
 public class PhoneNumber {
     
-    private long code;
+    private short  code;
     private long number;
 
     public PhoneNumber() {
     }
 
-    public PhoneNumber(long code, long number) {
+    public PhoneNumber(short code, long number) {
         this.code = code;
         this.number = number;
     }
 
-    public long getCode() {
+    public short getCode() {
         return code;
     }
 
-    public void setCode(long code) {
+    public void setCode(short code) {
         this.code = code;
     }
 
